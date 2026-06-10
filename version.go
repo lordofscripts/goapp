7f944d00ecd7c03e1c4b1082506a5b3c55d96c45
Lord of Scripts ™ <8118933+lordofscripts@users.noreply.github.com>
2026-06-10 16:01:03 -0500

    Add module version descriptor

---
+++ version.go
@@ -0,0 +1,30 @@
+/* -----------------------------------------------------------------
+ *              L o r d  O f   S c r i p t s (tm)
+ *             Copyright (C)2026 Dídimo Grimaldo T.
+ *                         goApp Module
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ * Module version descriptor. Useful if the client application needs
+ * to send problem reports by querying each of my module's version
+ * as in this case goapp.ModuleVersion.String()
+ *-----------------------------------------------------------------*/
+package goapp
+
+import "github.com/lordofscripts/goapp/app"
+
+/* ----------------------------------------------------------------
+ *                       G L O B A L S
+ *-----------------------------------------------------------------*/
+
+const (
+	_NAME    string = "goApp"
+	_DESC    string = "a minimal GO CLI mini-framework"
+	_VERSION string = "1.3.3"
+)
+
+var (
+	ModuleVersion app.PackageVersion = app.NewReleaseVersion(_NAME, _DESC, _VERSION)
+)
+
+/* ----------------------------------------------------------------
+ *                       F U N C T I O N S
+ *-----------------------------------------------------------------*/
